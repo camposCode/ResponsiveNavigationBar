b0b0ee08177033d3ced4a360ce0154f5761d3dd1
camposCode <info@josecampos.io>
2022-09-01 10:30:36 +0200

    container done, togglebox done, toggle functions done

---
+++ app.js
@@ -0,0 +1,6 @@
+let toggleClick = document.querySelector('.toggleBox');
+let container = document.querySelector('.container');
+toggleClick.addEventListener('click', () =>{
+    toggleClick.classList.toggle('active');
+    container.classList.toggle('active');
+})
